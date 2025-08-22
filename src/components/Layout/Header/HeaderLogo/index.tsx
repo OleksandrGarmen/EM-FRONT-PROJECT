25d77673d98da2c69d8458b604aeb343036a75a3
Nik91K <nikotakovsl@gmail.com>
2025-08-22 12:29:29 +0300

    Add header logo component

---
+++ src/components/Layout/Header/HeaderLogo/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+const HeaderLogo = () => {
+    return (
+        <div className='header-logo'>
+            <img src="https://placehold.co/66x40" alt="" />
+            <p className='header-titile'>Book heaven</p>
+        </div>
+    )
+}
+
+export default HeaderLogo
