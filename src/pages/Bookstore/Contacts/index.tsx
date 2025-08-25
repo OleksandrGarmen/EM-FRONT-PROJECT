512f418470f8f2b77525f8cf648f36ce8943180e
progenitor <korndavid10@gmail.com>
2025-08-25 15:08:00 +0300

    fix contacts

--- src/pages/Bookstore/Contacts/index.tsx
+++ src/pages/Bookstore/Contacts/index.tsx
@@ -23,7 +23,7 @@ const Contacts = () => {
                     <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2630.7774178370577!2d32.07205291596175!3d49.444437979348454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d14e3e67c230f5%3A0x831d3b3bfa0b4a65!2z0KHRgtGA0L7QtdC90LjRhtC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNDQsINCh0L7QvdC-0YDQvtCz0LIsINCe0LrQu9Cw0YHRgtGMLCAyMDgwMA!5e0!3m2!1suk!2sua!4v1692714776982!5m2!1suk!2sua"
                     width="100%"
-                    height="400"></iframe>
+                    height="580"></iframe>
                 </div>
                 <div className="contact-button-container">
                     <a href="/" className="email-button">Написати нам</a>
