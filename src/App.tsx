325ae7e24843e6658d8168b18e9c6fac96509cac
Nik91K <nikotakovsl@gmail.com>
2025-08-26 08:46:10 +0300

    Add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import LayoutPage from './layout/layoutPage'
+import titleImage from './assets/4eb4199600309ef6a651f94f390ce288e7fbf77f.png'
+import SearchInput from './components/Common/Inputs/Search'
+import SubmitButton from './components/Common/Buttons/SubmitButton'
 import './App.css'
 
 function App() {
-
+    
   return (
     <LayoutPage>
-      <div>
-
+      <div className='title-container'>
+        <div className='title-container-left'>
+          <div>
+            <h2 className='title-container-title'>
+              Find your next great read at our online book store
+            </h2>
+            <p className='title-container-text'>
+              Explore our current collection
+            </p>
+          </div>
+          <div className='title-container-bottom'>
+            <SearchInput placeholder='Type your books here' backgroudColor='#9b9b9b' color='#000000' /><SubmitButton text='Search Now' />
+          </div>
+          
+        </div>
+        <div>
+          <img src={titleImage} alt="titleImage" className='title-image'/>
+        </div>
       </div>
     </LayoutPage>
   )
