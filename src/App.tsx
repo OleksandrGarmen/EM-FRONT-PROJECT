34528e0de2680d3f9c183022ca7f3c701a241172
Nik91K <nikotakovsl@gmail.com>
2025-08-26 13:05:38 +0300

    Changed main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import LayoutPage from './layout/layoutPage';
 import titleImage from './assets/4eb4199600309ef6a651f94f390ce288e7fbf77f.png';
 import SearchInput from './components/Common/Inputs/Search';
 import SubmitButton from './components/Common/Buttons/SubmitButton';
-import './App.css';
+import Book from './components/Book';
+import books from './fixture/books.json'
+import type { BookType } from './types/BookType';
+import authors from './fixture/authors.json'
+import type { AuthorType } from './types/AuthorType';
+
+import './App.css'
+let bookData:BookType[] = books
+let allAuthors:AuthorType[] = authors
 
 function App() {
   return (
@@ -11,7 +19,7 @@ function App() {
         <div className='title-background'></div>
         <div className='title-content-wrapper'>
           <div className='title-container-left'>
-            <div>
+            <div className='title-container-box'>
               <h2 className='title-container-title'>
                 Find your next great read at our online book store
               </h2>
@@ -20,7 +28,9 @@ function App() {
               </p>
             </div>
             <div className='title-container-bottom'>
-              <SearchInput placeholder='Type your books here' backgroudColor='#9b9b9b' color='#000000' /><SubmitButton text='Search Now' />
+              <div>
+                <SearchInput placeholder='Type your books here' backgroudColor='#9b9b9b' color='#000000' />
+              </div><SubmitButton text='Search Now' />
             </div>
           </div>
           <div>
@@ -28,8 +38,25 @@ function App() {
           </div>
         </div>
       </div>
+      <div>
+        <h2 className='popular-books-container'>
+          Popular Books
+        </h2>
+        <div className='popular-books'>
+          { bookData.map(element => {
+              const author = allAuthors.find(a => a.id === element.authorId);
+              return (
+                  <Book key={element.id} {...element}>
+                      <div className="author">
+                          {author ? author.name : "Невідомо"}
+                      </div>
+                  </Book>
+              )
+          })}
+        </div>
+      </div>
     </LayoutPage>
-  );
+  )
 }
 
 export default App;
