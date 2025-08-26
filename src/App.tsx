68fcf9291f117d2c264f2f7663359d086aa03caa
Nik91K <nikotakovsl@gmail.com>
2025-08-26 10:16:10 +0300

    Add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import LayoutPage from './layout/layoutPage'
-import titleImage from './assets/4eb4199600309ef6a651f94f390ce288e7fbf77f.png'
-import SearchInput from './components/Common/Inputs/Search'
-import SubmitButton from './components/Common/Buttons/SubmitButton'
-import './App.css'
+import LayoutPage from './layout/layoutPage';
+import titleImage from './assets/4eb4199600309ef6a651f94f390ce288e7fbf77f.png';
+import SearchInput from './components/Common/Inputs/Search';
+import SubmitButton from './components/Common/Buttons/SubmitButton';
+import './App.css';
 
 function App() {
-    
   return (
-    <LayoutPage>
+    <LayoutPage pageClass="home-page">
       <div className='title-container'>
-        <div className='title-container-left'>
+        <div className='title-background'></div>
+        <div className='title-content-wrapper'>
+          <div className='title-container-left'>
+            <div>
+              <h2 className='title-container-title'>
+                Find your next great read at our online book store
+              </h2>
+              <p className='title-container-text'>
+                Explore our current collection
+              </p>
+            </div>
+            <div className='title-container-bottom'>
+              <SearchInput placeholder='Type your books here' backgroudColor='#9b9b9b' color='#000000' /><SubmitButton text='Search Now' />
+            </div>
+          </div>
           <div>
-            <h2 className='title-container-title'>
-              Find your next great read at our online book store
-            </h2>
-            <p className='title-container-text'>
-              Explore our current collection
-            </p>
+            <img src={titleImage} alt="titleImage" className='title-image' />
           </div>
-          <div className='title-container-bottom'>
-            <SearchInput placeholder='Type your books here' backgroudColor='#9b9b9b' color='#000000' /><SubmitButton text='Search Now' />
-          </div>
-          
-        </div>
-        <div>
-          <img src={titleImage} alt="titleImage" className='title-image'/>
         </div>
       </div>
     </LayoutPage>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/layout/layoutPage.tsx
+++ src/layout/layoutPage.tsx
@@ -2,9 +2,9 @@ import './style.css'
 import ResponsiveHeader from '../components/Layout/Header/ResponsiveHeader'
 import Footer from '../components/Layout/Footer/FooterComponent'
 
-const LayoutPage = ({children} : {children:React.ReactNode}) => {
+const LayoutPage = ({children, pageClass} : {children:React.ReactNode, pageClass?: string}) => {
     return (
-        <div className='layout-page'>
+        <div className={`layout-page ${pageClass || ''}`}>
             <ResponsiveHeader />
             <div className='content'>
                 {children}
